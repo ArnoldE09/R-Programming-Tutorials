da17133e68c46019fb0cebe93eed9716018ff7aa
Arnold C. Estorba <ArnoldProg@gmail.com>
2023-04-28 09:42:24 +0800

    Update Tutorials

---
+++ R-Tutorials.r
@@ -0,0 +1,56 @@
+Getting and Setting the Working Directory
+# Get and print current working directory.
+print(getwd())
+
+# Set current working directory.
+setwd("/web/com")
+
+# Get and print current working directory.
+print(getwd())
+
+data <- read.csv("input.csv")
+print(data)
+
+Analyzing the CSV File
+
+data <- read.csv("input.csv")
+
+print(is.data.frame(data))
+print(ncol(data))
+print(nrow(data))
+
+Get the maximum salary
+# Create a data frame.
+data <- read.csv("input.csv")
+
+# Get the max salary from data frame.
+sal <- max(data$salary)
+print(sal)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+# Get the max salary from data frame.
+sal <- max(data$salary)
+
+# Get the person detail having max salary.
+retval <- subset(data, salary == max(salary))
+print(retval)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+retval <- subset( data, dept == "IT")
+print(retval)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+info <- subset(data, salary > 600 & dept == "IT")
+print(info)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+print(retval)
